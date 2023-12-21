176bed179fbea1fb6ec256b2eccf59857288c9b6
Gourav Panseja <gouravpanseja25@gmail.com>
2023-12-21 20:51:18 +0530

    s

--- src/components/Auth.js
+++ src/components/Auth.js
@@ -1,37 +1,56 @@
 import { useState,useRef } from "react";
 import {auth,googleProvider} from "../config/firebase"
-import { createUserWithEmailAndPassword,signInWithPopup,signOut } from "firebase/auth";
+import { createUserWithEmailAndPassword,signInWithPopup,signOut,signInWithEmailAndPassword } from "firebase/auth";
 function Auth(){
 
     const [email,setEmail] = useState("");
     const [password,setPassword] = useState("");
     const display = useRef();
 
-    const displayText =()=>{ 
-        display.current.innerHTML = (auth?.currentUser?.email) ? `User email: ${auth.currentUser.email}` : "None";
-        console.log(auth?.currentUser?.photoURL)
+    const displayText =(text)=>{ 
+        display.current.innerHTML = text
+        
     }
-    const signInWithGoogle= async ()=>{
+    const signUpwithGoogle= async ()=>{
         try{
             await signInWithPopup(auth,googleProvider);
+            displayText(`signedUp with google ${email} `)
         } 
         catch(e){
-            console.log(e);
+            displayText(e);
         }
-      
+
     }
-    const clickHandler= async (e)=>{
+    const signUp= async (e)=>{
         try{
             await createUserWithEmailAndPassword(auth,email,password);
+            displayText(`signedUp as ${email} `)
            
 
         }
         catch(error){
-            console.log(error)
+            displayText(error)
         }
     }
     const logOut = async ()=>{
-        await signOut(auth);
+        try{
+            await signOut(auth);
+            displayText("logged out")
+        }
+        catch(error){
+            displayText(error)
+        }
+        
+    }
+    const signIn = async()=>{
+        try{
+            await signInWithEmailAndPassword(auth,email,password);
+            displayText(`signed in as ${email}`)
+        }
+        catch(error){
+            displayText(error)
+        }
+        
     }
     return(
         <div>
@@ -42,11 +61,10 @@ function Auth(){
             </div>
             <br/>
             <div>
-                <button onClick={clickHandler}>Sign In</button>
+                <button onClick={signIn}>signIn</button>
+                <button onClick={signUp}>Sign Up</button>
                 <br/>
-                <button onClick={displayText}>Display user</button>
-                <br/>
-                <button onClick={signInWithGoogle}>Sign in with Google!</button>
+                <button onClick={signUpwithGoogle}>Sign up with Google!</button>
                 <br/>
                 <button onClick={logOut}>Logout</button>
             </div>
